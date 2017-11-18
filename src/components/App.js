8de82102137274ac019af32952286b74593cb195
Marcin Domański <marcin@domanski.eu>
2017-11-18 09:53:59 +0100

    added components Header.js and Footer.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Header from './Header';
+import Footer from './Footer';
 
 const App = () => {
     return (<div>
+        <Header/>
         <h1>Hello world</h1>
         <p>Lorem ipsum</p>
+        <Footer/>
     </div>);
 };
 
