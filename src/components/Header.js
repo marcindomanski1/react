adf7564f7f14ce7b2c3a5c23a26d3c272478d9c5
Marcin Domański <marcin@domanski.eu>
2017-11-18 10:14:43 +0100

    added components Menu.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import Menu from './Menu';
 
 const Header = () => {
     return (<div>
         <h1>Header</h1>
+        <Menu/>
     </div>);
 };
 
---
+++ src/components/Menu.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Menu = () => {
+    return (
+        <ul>
+            <li>Home</li>
+            <li>Profile</li>
+            <li>Dashboard</li>
+        </ul>
+    );
+};
+
+export default Menu;
