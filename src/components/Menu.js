8a758a07dd04b82901e3323f96e2d55374e03ff5
Marcin Domański <marcin@domanski.eu>
2017-11-18 10:33:13 +0100

    modified component Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
 const Menu = () => {
+    // const menuElements = [
+    //     <li key="1">Home</li>,
+    //     <li key="2">Profile</li>,
+    //     <li key="3">Dashboard</li>
+    // ];
+
+    const menuElements = ['Home', 'Profile', 'Dashboard'].map((element, index) => {
+        return <li key={index}>{element}</li>;
+    });
+
     return (
         <ul>
-            <li>Home</li>
-            <li>Profile</li>
-            <li>Dashboard</li>
+            {menuElements}
         </ul>
     );
 };
