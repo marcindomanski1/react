403f463d8c252fe843bdf62de0404b025e80928d
Marcin Domański <marcin@domanski.eu>
2017-11-18 11:01:48 +0100

    added style to Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Menu from './Menu';
 import logo from '../logo.svg';
+// import './logo.css';
+
+const logoStyle = {
+    width: '100px'
+};
 
 const Header = () => {
-    return (<div>
+    return (<div className="header">
         <h1>Header</h1>
-        <img src={logo}/>
+        {/*<img className="logo" src={logo}/>*/}
+        <img style={logoStyle} src={logo}/>
         <Menu/>
     </div>);
 };
